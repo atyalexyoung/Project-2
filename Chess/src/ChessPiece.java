8b94f4f126cc0aa8f278dc2a5826230e9683bc84
GamingGuitarMan <atyalexyoung@gmail.com>
2022-02-17 18:58:13 -0500

    Finished ChessPiece Class ???????

--- Chess/src/ChessPiece.java
+++ Chess/src/ChessPiece.java
@@ -13,114 +13,38 @@ public abstract class ChessPiece {
 	}
 
 	public boolean isValidMove(Move move, ChessPiece[][] board) {
-		//TODO: implement this method
+		
+		// move is set to invalid unless it meets the critera met in "if-statements" below
+		boolean validMove = false;
+		
+		// creating new variables for the place the current player is trying to move from 
+		// and the place the current player is trying to move to
+		ChessPiece from = board[move.fromRow][move.fromColumn];
+		ChessPiece to = board[move.toRow][move.toColumn];
 
-
-
-
-
-	ChessPiece startingPos[][] = new ChessPiece[move.fromRow][move.fromColumn];
-	ChessPiece endingPos[][] = new ChessPiece[move.toRow][move.toColumn];
-	 
-	if(startingPos == board)
-	{
-		if(move.toColumn <= 8 && move.toRow <= 8)
+		// checks if the place you are trying to move is in bounds
+		if(move.toColumn <= 8 && move.toColumn >=0 && move.toRow >= 0 && move.toRow <= 8 )
 		{
-			if(startingPos != endingPos)
-			{
-				if(endingPos == null)
+			// checks if the place the current player is trying to move is not the place the current player is staring from
+			if(from != to)
+			{	
+				// checks if the place the current player is trying to move from has a piece that belongs to the current player
+				if(from != null &&  from.owner == this.owner)
 				{
-					return true;
+					// checks if the place the current player is trying to move has a piece already there that is the current owners 
+					if(from.owner != this.owner)
+						validMove = true;
+					else{/* the place the current player is trying to move has a piece that is the current owners already */ }
 				}
-				else
-				{
-					if(board.player() == endingPos.player())
-					{
-						
-					}
-				}
+				else{/* there is not a piece that is the current players at the place the current player is trying to move from */}
 			}
-			else
-			{
-				
-			}
+			else{/* the place the current player is trying to move to is the place the current player is trying to move from */}
 		}
-	} 
+		else{/* the move the current player is trying to make is out of bounds */}
 
-
-
-
-
-	/*
-	try
-	{	
-		// Rows array for loop
-		for(int i = 0; i <= 8; i++)
-		{
-			if (i > 8)
-			{
-				throw new IllegalArgumentException("Out of bounds");
-			}
-			else
-			{
-
-			}
-			// Columns array for loop
-			for(int k = 0; k <= 8; k++)
-			{
-				if(k > 8)
-				{
-					throw new IllegalArgumentException("Out of bounds");
-				}
-				else
-				{
-
-				}
-			}
-		}
-	}
-	catch(IndexOutOfBoundsException oof)
-	{
-		System.out.println("Invalid piece placement");
-	}
+		// returns boolean if the move is valid or not
+		return validMove;
 
 	
-	if(board[move.toRow][move.toColumn] == null){
-
-				}
-	*/
-
-		/*
-
-		if (piece == starting place in move){
-			if(piece === startingPos[][=])
-			if (move(newspace) != current space){
-
-				if (rows < 8 && columns < 8){
-				
-					if (move(newspace) != another piece from same player){
-						
-						return true;
-
-		}
-		else{
-			throw new exception
-		}
-		}
-		else{
-			throw new exception
-		}
-		}
-		else{
-			throw new exception
-		}
-		}
-		else {
-			throw new exception
-		}
-
-
-
-		*/
 	}
 }
