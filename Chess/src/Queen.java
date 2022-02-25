3d96a6f8acd3966efebd8eaaf3b762ce1978376f
owen <wheeleow@mail.gvsu.edu>
2022-02-25 17:49:40 -0500

    bishop nearly done

--- Chess/src/Queen.java
+++ Chess/src/Queen.java
@@ -1,3 +1,5 @@
+import static java.lang.Math.abs;
+
 public class Queen extends ChessPiece {
 
 	public Queen(Player player) {
@@ -7,26 +9,76 @@ public class Queen extends ChessPiece {
 	public String type() {
 		return "Queen";
 	}
+	private boolean isQueenValidMove(Move move, ChessPiece[][] board){
+		boolean valid = false;
+		int rowDiff = move.toRow - move.fromRow;
+		int colDiff = move.toColumn - move.fromColumn;
+		int inc;
+		//checks if queen is moving in same row/column (not diagonally)
+		if(abs(rowDiff) != abs(colDiff)){
+		if(abs(rowDiff) > 0){
+			//checks if queen is increasing or decreasing rows
+			if(rowDiff > 0){
+				inc = 1;
+			}else{
+				inc = -1;
+			}
+			for(int i = move.fromRow + inc; i != move.toRow; i += inc){
+				if(board[i][move.fromColumn] == null){
+					valid = true;
+				}
+				else{
+					valid = false;
+					break;
+				}
+			}
+			if (abs(rowDiff) == 1){
+				valid = true;
+			}
+			if(abs(colDiff) != 0){
+				valid = false;
+			}
+		}
+		//checks if queen is moving left to right
+		if(abs(colDiff) > 0){
+			//checks if queen is increasing or decreasing rows
+			if(colDiff > 0){
+				inc = 1;
+			}else{
+				inc = -1;
+			}
+			for(int i = move.fromColumn + inc; i != move.toColumn; i += inc){
+				if(board[move.fromRow][i] == null){
+					valid = true;
+				}
+				else{
+					valid = false;
+					break;
+				}
+			}
+			if (abs(colDiff) == 1){
+				valid = true;
+			}
+			if(abs(rowDiff) != 0){
+				valid = false;
+			}
+		}
+	}
+		return valid;
+	}
 	
 	public boolean isValidMove(Move move, ChessPiece[][] board) {
-		// TODO:  implement this method
-		// Think carefully about how you could do this
-
-		/*
-		
-		if (super isValidMove == true)
-			if (rook isValidMove == true || bishop isValidMove == true)
+		if(super.isValidMove(move, board) == true){
+			if(isQueenValidMove(move, board) == true){
 				return true;
-			else
+			}
+			else{
 				return false;
-
-		else
+			}
+		}
+		else{
 			return false;
-
-
-
-
-		*/
-		return true;
+		}
+	
 	}
 }
